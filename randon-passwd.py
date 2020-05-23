4a30f730ed5763bc0f895c87fe5dcbbf6abb364b
Anis KHACHNAOUI <aniskhachnawi@gmail.com>
2020-05-24 01:02:13 +0200

    randon-passwd.py

---
+++ randon-passwd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#Python script to generate a random password with 12-16 characters
+
+import string
+import random
+
+def random_passwd():
+    #choose password lengh
+    pwd_length = random.randint(12,16)
+    #define possible alphanumeric to use
+    alpha_list = "0123456789#$%&()*+,-./:;<=>?@[\]^_{|}~"
+    # append alphabetic list
+    characters_list = alpha_list + string.ascii_lowercase + string.ascii_uppercase
+    #generate password
+    password = ''
+    for i in range (1, pwd_length):
+        password = password + characters_list[random.randint(1,len(characters_list))]
+    print (password)
+def main():
+    random_passwd()
+
+if __name__ == "__main__":
+    main()
